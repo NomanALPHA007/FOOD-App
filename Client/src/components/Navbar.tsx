545f0fd3153a4e23a106e835313eb7196c9b50d3
Arnobrizwan <arnobrizwan23@gmail.com>
2024-12-15 19:43:43 +0800

    c
    c

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom";
 import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "./ui/menubar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
-import { Loader2, Moon, ShoppingCart, Sun } from "lucide-react";
+import { Loader2, Menu, Moon, ShoppingCart, Sun, User } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Separator } from "./ui/separator";
 
 const Navbar = () => {
   const admin = true;
@@ -22,75 +25,76 @@ const Navbar = () => {
             <Link to="/">Home</Link>
             <Link to="/profile">Profile</Link>
             <Link to="/order/status">Order</Link>
-          </div>
-          {
-            admin && (
-              <Menubar>
-                <MenubarMenu><MenubarTrigger>
-                  Dashboard
-                </MenubarTrigger>
-                  <MenubarContent>
-                    <Link to="/admin/restaurant">
-                      <MenubarItem>Restaurant</MenubarItem></Link>
-                    <Link to="/admin/menu">
-                      <MenubarItem>Menu</MenubarItem></Link>
-                    <Link to="/admin/orders">
-                      <MenubarItem>Orders</MenubarItem></Link>
-                  </MenubarContent>
-                </MenubarMenu>
 
-              </Menubar>
-            )
-          }
-        </div>
-        <div className="flex items-center gap-4">
-          <div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Toggle theme</span>
+            {
+              admin && (
+                <Menubar>
+                  <MenubarMenu><MenubarTrigger>
+                    Dashboard
+                  </MenubarTrigger>
+                    <MenubarContent>
+                      <Link to="/admin/restaurant">
+                        <MenubarItem>Restaurant</MenubarItem></Link>
+                      <Link to="/admin/menu">
+                        <MenubarItem>Menu</MenubarItem></Link>
+                      <Link to="/admin/orders">
+                        <MenubarItem>Orders</MenubarItem></Link>
+                    </MenubarContent>
+                  </MenubarMenu>
+
+                </Menubar>
+              )
+            }
+          </div>
+          <div className="flex items-center gap-4">
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" size="icon">
+                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Toggle theme</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem >
+                    Light
+                  </DropdownMenuItem>
+                  <DropdownMenuItem >
+                    Dark
+                  </DropdownMenuItem>
+
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <Link to="/cart" className="relative cursor-pointer">
+              <ShoppingCart />
+              <Button size={'icon'} className="absolute -inset-y-3 left-2 text-xs rounded-full h-4 w-4 bg-red-500 hover:bg-red-500">2</Button>
+            </Link>
+            <div>
+              <Avatar>
+                <AvatarImage />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            {
+              loading ? (
+                <Button className="bg-orange hover:bg-hoverOrange">
+
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please Wait
                 </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem >
-                  Light
-                </DropdownMenuItem>
-                <DropdownMenuItem >
-                  Dark
-                </DropdownMenuItem>
+              ) : (
+                <Button className="bg-orange hover:bg-hoverOrange">Logout
+                </Button>
+              )
+            }
 
-              </DropdownMenuContent>
-            </DropdownMenu>
           </div>
-          <Link to="/cart" className="relative cursor-pointer">
-            <ShoppingCart />
-            <Button size={'icon'} className="absolute -inset-y-3 left-2 text-xs rounded-full h-4 w-4 bg-red-500 hover:bg-red-500">2</Button>
-          </Link>
-          <div>
-            <Avatar>
-              <AvatarImage />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </div>
-          {
-            loading ? (
-              <Button className="bg-orange hover:bg-hoverOrange">
-
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Please Wait
-              </Button>
-            ) : (
-              <Button className="bg-orange hover:bg-hoverOrange">Logout
-              </Button>
-            )
-          }
-
         </div>
         <div className="md:hidden lg:hidden">
           {/*Mobile responsive*/}
-          <MobileNavbar/>
+          <MobileNavbar />
         </div>
 
       </div>
@@ -105,17 +109,74 @@ export default Navbar;
 const MobileNavbar = () => {
   return (
     <Sheet>
-      <SheetTrigger>Open</SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
-          </SheetDescription>
+      <SheetTrigger asChild>
+        <Button size={'icon'} className="rounded-full bg-gray-200 text-black hover:bg-gray" variant="outline">
+          <Menu size={'18'} />
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent className="flex flex-col">
+        <SheetHeader className="flex flex-row items-center justify-between mt-2">
+          <SheetTitle>FoodTiger</SheetTitle>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem >
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem >
+                Dark
+              </DropdownMenuItem>
+
+            </DropdownMenuContent>
+          </DropdownMenu>
+
         </SheetHeader>
+        <Separator className="my-2" />
+        <SheetDescription className="flex-1">
+          <Link to="/profile" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
+            <User />
+            <span>Profile</span>
+          </Link>
+          <Link to="/profile" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
+            <User />
+            <span>Profile</span>
+          </Link>
+          <Link to="/profile" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
+            <User />
+            <span>Profile</span>
+          </Link>
+          <Link to="/profile" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
+            <User />
+            <span>Profile</span>
+          </Link>
+          <Link to="/profile" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
+            <User />
+            <span>Profile</span>
+          </Link>
+          <Link to="/profile" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
+            <User />
+            <span>Profile</span>
+          </Link>
+          <Link to="/profile" className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium">
+            <User />
+            <span>Profile</span>
+          </Link>
+        </SheetDescription>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit" className="bg-orange hover:bg-hoverOrange">Logout</Button>
+          </SheetClose>
+        </SheetFooter>
+
       </SheetContent>
     </Sheet>
 
   )
-}
+} 
