c09d739826baa778b363e57cc00e6bef48153072
Ehtasham Al Noman <ehtasham@graduate.utm.my>
2024-12-16 11:25:52 +0800

    Noman-sp1

--- Client/src/layout/MainLayout.tsx
+++ Client/src/layout/MainLayout.tsx
@@ -1,19 +1,25 @@
-
-import { Outlet } from "react-router-dom"
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen m-2 md:m-0">
-        {/* Navbar  */}
-        
-        {/* Main content  */} 
-        <div className="flex-1">
-            <Outlet/>
-        </div>
+      {/* Navbar  */}
+      <header>
+        <Navbar />
+      </header>
+      {/* Main content  */}
+      <div className="flex-1">
+        <Outlet />
+      </div>
 
-        {/* Footer  */}
+      {/* Footer  */}
+      <footer>
+        <Footer />
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
